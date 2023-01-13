cc1a863740ec005f2e13d45a3d98e0d73ea6e581
Théodore Diouf <92777850+Tdiouf10@users.noreply.github.com>
2023-01-13 11:24:41 +0100

    first commit backend

--- petite-annonce-backend-nest/src/app.module.ts
+++ petite-annonce-backend-nest/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule} from "@nestjs/mongoose";
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  
+  imports: [
+      MongooseModule.forRoot(
+          "mongodb+srv://rootAccess:UfsREETshSn9igN@monlogement.wdjryvj.mongodb.net/?retryWrites=true&w=majority"
+      ),
+      UsersModule,
+      AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
